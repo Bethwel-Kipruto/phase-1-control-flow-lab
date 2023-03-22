a2241c1d13b4ef9c8e6e4e3c50f32ba21f76da14
BETHWEL <bethwelk85@gmail.com>
2023-03-22 08:38:21 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if(distance <= 400) {
+    return 'This one is on me!';
+  }
+  else if(distance>2000 && distance<=2499){
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if(distance> 2500){
+    return 'No can do.';
+  }
+}
+function ternaryCheckCity(city){
+  // Write your code here!
+  if(city=="NYC"){
+    return 'Ok, sounds good.';
+  }
+  else if(city!=="NYC"){
+    return 'No go.';
+  }
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch(tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.';
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
